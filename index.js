2e441dbd1ee14612cd784e1871004b5649e1c081
Rishabh Das <rishabhdasufl@gmail.com>
2021-08-03 00:39:14 +0530

    Clears Console on all OS

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+
+const clearConsole = require("clear-any-console");
+
+// clears the console in all Operating Systems.
+clearConsole();
+
 console.log(
-  `Hi Rishabh (aka "up16") here 👋!! Thanks for trying out up16-cli.`
+  `Hi Rishabh (aka "up16") here 👋!! Thanks for trying out up16-cli. Meow`
 );
